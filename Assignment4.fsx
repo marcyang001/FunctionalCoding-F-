4c2a45c30e1045da5a04ddd89c9509f7c8639a42
Marc Yang <m.y.yang001@gmail.com>
2016-03-21 00:39:59 -0400

    a little adjustment

--- Assignment4.fsx
+++ Assignment4.fsx
@@ -55,21 +55,21 @@ let rec unify (tau1: typExp) (tau2:typExp) : substitution =
     | TypInt, TypInt -> []
     | TypInt, TypVar b -> [(b, TypInt)]
     | TypInt, Arrow(x, y) -> failwith "Not unifiable"
-    | TypInt, Lst x ->  if (x = TypInt) then failwith "Clash in principal type constructor" else failwith "Not unifiable"
+    | TypInt, Lst x ->  failwith "Not unifiable"
     | TypVar a, TypInt -> [(a, TypInt)]
     | TypVar a, TypVar b -> [(a, TypVar b)]
     | TypVar a, Arrow(x, y) -> if (occurCheck (a) (Arrow(x, y)) = false) then [(a, Arrow(x, y))] else failwith "Failed occurs check"
     | TypVar a, Lst x -> if (occurCheck (a) x = false) then [(a, Lst x)] else failwith "Failed occurs check"
     | Arrow(x, y), TypInt ->  failwith "Not unifiable"
     | Arrow(x, y), TypVar b ->  if (occurCheck (b) (Arrow(x, y)) = false) then [(b, Arrow(x, y))] else failwith "Failed occurs check"
-    | Arrow(x, y), Lst z ->  if (z = Arrow(x, y)) then failwith "Clash in principal type constructor" else failwith "Not unifiable"
+    | Arrow(x, y), Lst z ->  failwith "Clash in principal type constructor"
     | Arrow(x1, y1), Arrow(x2, y2) -> let firstHalf = unify(x1) (x2)
                                       let newExp1 = applySubst (firstHalf) y1
                                       let newExp2 = applySubst (firstHalf) y2
                                       unify newExp1 newExp2 @ firstHalf 
-    | Lst z, TypInt -> if (z = TypInt) then failwith "Clash in principal type constructor" else failwith "Not unifiable"
+    | Lst z, TypInt -> failwith "Not unifiable"
     | Lst z, TypVar b -> if (occurCheck (b) z = false) then [(b, Lst z)] else failwith "Failed occurs check"
-    | Lst z, Arrow(x, y) -> if (z = Arrow(x, y)) then failwith "Clash in principal type constructor" else failwith "Not unifiable"
+    | Lst z, Arrow(x, y) -> failwith "Clash in principal type constructor"
     | Lst z, Lst y -> unify (z) (y) 
 
 let te3 = Arrow(TypVar 'a',Arrow (TypVar 'b',TypVar 'c'))
@@ -116,33 +116,4 @@ val it : typExp = Prod (TypInt,Arrow (TypInt,TypInt))
 val it : typExp = Prod (TypInt,Arrow (TypInt,TypInt))
 
 *)
-
-
-    
-
-    
-(*
-
-> let te4 = Prod(TypInt, Arrow(TypVar 'c', TypVar 'a'));;
-
-val te4 : typExp = Prod (TypInt,Arrow (TypVar 'c',TypVar 'a'))
-
-> let te3 = Prod (TypVar 'a',Arrow (TypVar 'b',TypVar 'c'));;
-
-val te3 : typExp = Prod (TypVar 'a',Arrow (TypVar 'b',TypVar 'c'))
-
-> unify te3 te4;;
-val it : substitution = [('c', TypInt); ('b', TypVar 'c'); ('a', TypInt)]
-> let result = it;;
-
-val result : substitution = [('c', TypInt); ('b', TypVar 'c'); ('a', TypInt)]
-
-> applySubst result te3;;
-val it : typExp = Prod (TypInt,Arrow (TypInt,TypInt))
-> applySubst result te4;;
-val it : typExp = Prod (TypInt,Arrow (TypInt,TypInt))
-
-*)
-
-
   
